7925c2214e3e8c6553b77205a6279ff446ef0756
Mayank Agarwal <7mayank.agarwal@gmail.com>
2023-06-15 17:23:52 +0530

    create quiz

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,10 +1,29 @@
 import { Router } from "express";
+import Quiz from "../models/quiz.js";
 
 const router=Router()
 
-router.post('/',(req,res)=>
+router.post('/',async(req,res)=>
 {
-  res.send('quiz')
+  const {question,options,rightAnswer,startDate,endDate}=req.body
+  let quiz=new Quiz({question,options,rightAnswer,startDate,endDate})
+  let errors=quiz.validateSync()
+  if(errors) return res.json(errors)
+
+  const startTime=new Date(startDate).getTime()
+  const endTime=new Date(endDate).getTime()
+  if(endTime<startTime+15*60*1e3 || endTime>startTime+60*60*1e3) return res.json({error: 'Quiz duration should be between 15 and 60 mins'})
+
+  try
+  {
+    quiz = await quiz.save()
+    res.json({data: quiz})
+  }
+  catch(e)
+  {
+    console.error(e);
+    res.json({error:'Internal server error'})
+  }
 })
 
 export {router as quizRouter}
--- models/quiz.js
+++ models/quiz.js
@@ -1,28 +1,41 @@
-import { Model, Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 
 const schema=new Schema({
   question: {
     type: String,
-    required: true
+    required: [true,'Question title is required']
   },
   options: {
     type: [String],
-    required: true
+    required: [true,'Options are required'],
+    validate:
+    {
+      validator: (options) => options.length>1,
+      message: 'Provide at least 2 options'
+    }
   },
   rightAnswer: {
     type: Number,
-    required: true
+    required: [true,'Right answer is required']
   },
   startDate: {
     type: Date,
-    required: true
+    required: true,
+    validate:
+    {
+      validator: (d) => d>=Date.now()+5*60*1e3,
+      message: 'Start date should be given at least 5 mins from now'
+    }
   },
   endDate: {
     type: Date,
     required: true
   },
-  status: String
+  status: {
+    type: String,
+    default: 'inactive'
+  }
 })
 
-const Quiz=Model('Quiz',schema)
+const Quiz=model('Quiz',schema)
 export default Quiz
